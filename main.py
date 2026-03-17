e4b2e74e22d522d93cda3bcd1ccc11899e730bab
szymonwatroba07-tech <szymonwatroba07@gmail.com>
2026-03-17 14:30:49 +0000

    added softmax and sigmoid(not working)

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ def l_relu(x):
         return -0.0001*x
     else:
         return x
+    
+def sigmoid_func(output):
+        for i in range(len(output)):
+            sigmoid_output = (1 / (1 + np.exp(-output)))
+        return sigmoid_output
 
-def normalize(outputs):
-    max = np.max(outputs)
-    return outputs/max
 
-
-inputs = np.random.rand(3)
+'''
+inputs = np.random.rand(2)
 weight_matrix = np.random.rand(3,3)
 biases = np.random.rand(3)
 
@@ -22,21 +24,44 @@ for i in range(len(outputs)):
 
 outputs = normalize(outputs)
 print(outputs)
-
+'''
 class Layer_Dense:
-    def __init__(self, n_inputs, n_outputs):
-        self.weight = np.random.rand(n_outputs, n_inputs)
-        self.biases = np.random.rand(n_outputs)
+    def __init__(self, input_size, output_size):
+        self.weight = np.random.rand(output_size, input_size)
+        self.biases = np.random.rand(output_size)
+#weight[i,j] is a weight betweenn previous layer i-th neuron and current
+#layer j-th neuron
 
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weight.T) + self.biases
+    def sigmoid(self):
+        self.sigmoid_output = 1 / (1 + np.exp(-self.output))
+        return self.sigmoid_output
 
-layer_1 = Layer_Dense(3,3)
-print(layer_1.weight, layer_1.biases)
-layer_1.forward(3)
-outputs = normalize(layer_1.output)
-print(outputs)
-layer_2 = Layer_Dense(len(outputs), 3)
-layer_2.forward(len(outputs))
-outputs = normalize(layer_2.output)
-print(outputs)
+
+
+X = np.random.rand(2)
+y_expected = [0,1]
+print(X, "\n")
+layer1 = Layer_Dense(2,4)  
+layer1.forward(X)
+layer1.sigmoid()
+layer2 = Layer_Dense(4,4)
+layer2.forward(layer1.output)
+layer2.sigmoid()
+y = Layer_Dense(4,2)
+y.forward(layer2.output)
+y.sigmoid()
+print(y.sigmoid_output,"\n")
+Z = np.exp(y.output)
+print(Z)
+
+def softmax(output):
+    exp_sum = 0.0
+    probability = []
+    for i in range(len(output)):
+        exp_sum += (float)(np.exp(output[i]))
+        probability.append((float)((np.exp(output[i]))/(exp_sum)))
+    return probability
+
+print(softmax(y.sigmoid_output))
