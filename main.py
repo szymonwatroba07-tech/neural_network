dfad3bc7d8b16aa4f23ffc7867b9436d7d237c2a
szymonwatroba07-tech <szymonwatroba07@gmail.com>
2026-03-19 16:08:55 +0000

    defined backward method and implemented it for last layer; however the weight matrix does not change :ccc

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
 import numpy as np
 
-def l_relu(x):
-    return np.where(x>0, x, -0.00001*x)
-    
-def sigmoid_func(output):
-    return 1 / (1 + np.exp(-output))
+def leaky_ReLU(output):
+    return np.where(output>0, output, 1e-9 *output)
 
 def softmax(output):
     exp_output = np.exp(output)
     exp_sum = np.sum(exp_output)
     return exp_output / exp_sum
 
+'''
 def Categorical_Cross_Entropy(y_expected, y_output):
     output_log = np.log(y_output)
     return (-np.dot(y_expected, output_log.T))
+'''
+
+def Mean_Square_Error(y_expected, y_output):
+    return np.mean((y_output - y_expected)**2)
+
 
 class Layer_Dense:
     def __init__(self, input_size, output_size):
         self.weight = 0.1*np.random.rand(output_size, input_size)
         self.biases = 0.1*np.random.rand(output_size)
-#weight[i,j] is a weight betweenn previous layer i-th neuron and current
-#layer j-th neuron
-
-    def forward(self, inputs, activation = None):
-        self.output = np.dot(inputs, self.weight.T) + self.biases
-        if activation == 'l_relu':
-            self.output = l_relu(self.output)
-        elif activation == 'sigmoid':
-            self.output = self.sigmoid()
+#weight[i,j] is a weight betweenn j-th neuron and previous'
+#layer i-th neuron
+    def forward(self, inputs):
+        self.inputs = inputs   
+        self.output = leaky_ReLU(np.dot(inputs, self.weight.T) + self.biases)
         return self.output
+    def backward(self, y_expected, y_output, learning_rate):
+        #dLoss_dWeights = dLoss_dOuput * dReLU_dWeightedSum * dWeightedSum_dWeights
+        dLoss_dOutput = 2 * (y_output - y_expected)                                 
+        dRelu_WeightedSum = np.where(self.output>0, 1, 1e-9)
+        dWeightedSum_dWeights = self.inputs
+        dLoss_dWeights = np.outer((dLoss_dOutput * dRelu_WeightedSum), dWeightedSum_dWeights.T)
+        self.dLoss_dWeights = dLoss_dWeights
+        self.weight -= (learning_rate * self.dLoss_dWeights)
+        return self.weight
+
+        
+    
     def sigmoid(self):
         self.sigmoid_output = 1 / (1 + np.exp(-self.output))
         return self.sigmoid_output
@@ -37,20 +48,26 @@ class Layer_Dense:
 
 X = np.random.rand(2)
 y_expected = [0,1]
+learning_rate = 0.1
 print(X, "\n")
 
 layer_1 = Layer_Dense(2,4)  
-layer_1.forward(X, activation = 'sigmoid')
+layer_1.forward(X)
 #print(layer_1.output, "\n")
 
 layer_2 = Layer_Dense(4,4)
-layer_2.forward(layer_1.output, activation = 'sigmoid')
+layer_2.forward(layer_1.output)
 #print(layer_2.output, "\n")
 
 y_layer = Layer_Dense(4,2)
 y_output = y_layer.forward(layer_2.output)
 
-loss = Categorical_Cross_Entropy(y_expected, y_output)
+#loss_CCE = Categorical_Cross_Entropy(y_expected, y_output)
+loss_MSE = Mean_Square_Error(y_expected, y_output) 
+
+new_weight_matrix = y_layer.backward(y_expected, y_output, 0.01)
 
 print(softmax(y_output))
-print("Loss: ", loss)
+print("Loss: ", loss_MSE)
+print("\nLast layer weight matrix: ", y_layer.weight)
+print("\nNew weight matrix: ", new_weight_matrix)
